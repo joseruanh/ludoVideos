7a29985bb6c8dfac5b4c83b5f46d46171b389766
joseruanh <joseruanh2@gmail.com>
2022-02-26 23:00:25 -0300

    subindo arquivos

---
+++ system/script.js
@@ -0,0 +1,85 @@
+function atualiza()
+{
+    location.reload()
+}
+
+function video1()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `
+    <iframe src="https://www.youtube.com/embed/TcKfYARcWEo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    `
+}
+
+function video2()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/k2FHq_pXElk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+
+function video3()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/1PaxK1HWsF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+
+function video4()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/9EstiZv_sj0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video5()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/rrXFBSD0nRw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video6()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/FsDA0LRBsrE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video7()
+{
+     let a = window.document.getElementById("video")
+    a.innerHTML = `
+    <iframe src="https://www.youtube.com/embed/XcgmDu0uTc0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+
+function video8()
+{
+     let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/4DpFox98kyg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video9()
+{
+     let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/zg4OOqw8BoU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video10()
+{
+     let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/1FOJpHdSgVk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video11()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/UWxP-ML7AeA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function video12()
+{
+    let a = window.document.getElementById("video")
+    a.innerHTML = `<iframe src="https://www.youtube.com/embed/m0xNuHHkZwc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+
